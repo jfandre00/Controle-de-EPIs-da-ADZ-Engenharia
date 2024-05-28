bc1ca3c7599d88075c751978d38d994077ce677e
Andre Loureiro Montini Ferreira <145864172+jfandre00@users.noreply.github.com>
2024-05-28 19:08:00 +0000

    alterando mais algumas cores e dando função aos botões

---
+++ script.js
@@ -0,0 +1,7 @@
+function registrarEntrega() {
+  let epi = document.getElementById("epi").value;
+  let quantidade = document.getElementById("quantidade").value;
+  let colaborador = document.getElementById("colaborador").value;
+  let cargo = document.getElementById("cargo").value;
+  let ca = document.getElementById("ca").value;
+}
